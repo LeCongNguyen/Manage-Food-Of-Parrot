4666c9bf565975df63c112efb46762ee25911373
Lê Nguyên <moingaymotlinhkien@gmail.com>
2022-11-10 22:06:30 +0700

    commit 5 - retest button

--- esp8266.ino
+++ esp8266.ino
@@ -20,14 +20,17 @@ String host = "managefoodofparrot.000webhostapp.com";
 String path = "/feeder-status.json";
 String status;
 String jsonData = "";
+int testBtn = 0; // button on Esp
+int i;
 
-//Keo's motor
-Stepper_28BYJ_48 keoStepper(16, 5, 4, 0); // tương ứng với D0. D1, D2, D3 của ESP8266
+// Keo's motor
+Stepper_28BYJ_48 keoStepper(5, 4, 2, 14); // tương ứng với D1, D4, D5 của ESP8266
 
 void setup()
 {
     Serial.begin(115200);
-    // Connect to Wifi (line 30 - 40)
+    pinMode(testBtn, INPUT_PULLUP);
+    // Connect to Wifi
     WiFi.begin(ssid, password);
     Serial.print("Connecting...");
     delay(1000);
@@ -43,21 +46,22 @@ void setup()
 
 void loop()
 {
-    // ESP sent request to Host to take json file (line 46 - 84)
+    // ESP sent request to Host to take json file
+    retest:
     if (WiFi.status() == WL_CONNECTED)
     {
-        // Connect to host through port 80 (line 49 - 53)
+        // Connect to host through port 80
         WiFiClient client;
         if (!client.connect(host, 80))
         {
             Serial.println("Connect to server fail!");
             return;
         }
-        // Sent GET request to host (line 56 - 58)
+        // Sent GET request to host
         client.print(String("GET ") + path + " HTTP/1.1\r\n" +
                      "Host: " + host + "\r\n" +
                      "Connection: keep-alive\r\n\r\n");
-        //Read data that host respond and show in serial monitor (line 50 to 83)
+        // Read data that host respond and show in serial monitor
         while (client.available() == 0)
         {
         }
@@ -82,7 +86,20 @@ void loop()
         Serial.print(".");
         delay(250);
     }
-    delay(120000);
+    //Tạo delay kết hợp kiểm tra nút nhấn
+    i = 120000;
+    while (i >= 0)
+    {
+        if (digitalRead(testBtn) == 0)
+        {
+            Serial.println("Retest...");
+            goto retest;
+        }
+        else {
+            i--;
+            delay(1);
+        }
+    }
 }
 
 void controlSteppers(String newData)
@@ -90,7 +107,7 @@ void controlSteppers(String newData)
     if (newData != jsonData)
     {
         jsonData = newData;
-        // Parse JSON (line 93 - 101)
+        // Parse JSON
         int size = 1024; // Bắt đầu từ ArduinoJson 6.7.0 DynamicJsonDocument có dung lượng cố định (tìm DynamicJsonDocument để xem thêm chi tiết)
         char json[size];
         newData.toCharArray(json, size); //Ép kiểu từ String sang Array
@@ -102,8 +119,7 @@ void controlSteppers(String newData)
         }
         // Keo's process
         int keoSteps = atoi(jsonBuffer["keoSet"]);
-        Serial.println(keoSteps);
-        if (strcmp(jsonBuffer["keo"], "on") == 0) //strcmp - string compare - so sánh 2 chuỗi, nếu bằng nhau thì trả về 0
+        if (strcmp(jsonBuffer["keo"], "on") == 0) // strcmp - string compare - so sánh 2 chuỗi, nếu bằng nhau thì trả về 0
         {
             keoStepper.step(keoSteps);
         }
